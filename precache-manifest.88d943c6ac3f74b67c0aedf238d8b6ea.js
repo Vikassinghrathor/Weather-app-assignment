4201ec89296ac1af9a9e3df5c55178b299369e60
vikassinghrathor <rathorevikas2809@gmail.com>
2024-02-26 02:49:13 +0530

    Updates

---
+++ precache-manifest.88d943c6ac3f74b67c0aedf238d8b6ea.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "660afd3326c395b59ad2e2238a183ff3",
+    "url": "/Weather-app-assignment/index.html"
+  },
+  {
+    "revision": "82c2d489ca7d71f4a6dd",
+    "url": "/Weather-app-assignment/static/css/main.691d16c7.chunk.css"
+  },
+  {
+    "revision": "3ddf6e37e7705cc2e1eb",
+    "url": "/Weather-app-assignment/static/js/2.c542ed1f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Weather-app-assignment/static/js/2.c542ed1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82c2d489ca7d71f4a6dd",
+    "url": "/Weather-app-assignment/static/js/main.5bba7232.chunk.js"
+  },
+  {
+    "revision": "bd9af6e4b4f2b66ef3df",
+    "url": "/Weather-app-assignment/static/js/runtime-main.d7e4fb0b.js"
+  },
+  {
+    "revision": "0a218296b39a3569fe9c933e3ababb49",
+    "url": "/Weather-app-assignment/static/media/WeatherIcons.0a218296.gif"
+  },
+  {
+    "revision": "75e0f8ca422777deab777d3abe0a90e7",
+    "url": "/Weather-app-assignment/static/media/background.75e0f8ca.jpg"
+  },
+  {
+    "revision": "e1698332435d6a6fb48294f85157aeab",
+    "url": "/Weather-app-assignment/static/media/city.e1698332.jpg"
+  }
+]);
